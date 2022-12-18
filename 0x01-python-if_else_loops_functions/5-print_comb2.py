68935d440344e7fee6316eda46361b77dd38f7e0
Your Name <you@example.com>
2022-12-17 22:16:23 -0800

    combination

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    print('{:02d}'.format(i), end='')
-    if i == 99:
-        break
+    if i < 99:
+        print('{:02d}'.format(i), end=', ')
     else:
-        print(', ', end='')
-print('\n')
+        print('{}\n'.format(i))
+
