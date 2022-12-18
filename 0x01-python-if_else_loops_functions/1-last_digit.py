f0bacae1bea9d7727e485d761e9874de6402cc46
Your Name <you@example.com>
2022-12-17 17:40:07 -0800

    if else function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,13 +12,12 @@ if number > 0:
         print('Last digit of', number, 'is', lastdigit1, 
                 'and is less than 6 and not 0')
 else:
-    number = str(number)
-    lastdigit = number[-1]
-    if lastdigit > '5':
+    lastdigit = number % -10
+    if lastdigit > -5:
         print('Last digit of', number, 'is', lastdigit, 
                 'and is greater than 5')
-    elif lastdigit == '0':
+    elif lastdigit == 0:
         print('last digit of', number, 'is', lastdigit, 'and is 0')
-    elif lastdigit < '6' and lastdigit != '0':
+    elif lastdigit < -6 and lastdigit != '0':
         print('Last digit of', number, 'is', lastdigit, 
                 'and is less than 6 and not 0')
