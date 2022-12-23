7621ea3b28c1f21ee5a7184b5343d23aa8121388
Your Name <you@example.com>
2022-12-22 20:45:45 -0800

    alphabets in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    char = i;
+    char = i
     if i % 2 != 0:
         char = char - 32
-    print('{:c}'.format(char), end ='')
+    print('{:c}'.format(char), end='')
