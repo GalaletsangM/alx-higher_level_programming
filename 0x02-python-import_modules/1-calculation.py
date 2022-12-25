ac8bd6c00a2a53f0d8750f02fc5bf7bc2c8cea63
Your Name <you@example.com>
2022-12-24 20:55:36 -0800

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator as cal
+    import calculator_1 as cal
     a = 10
     b = 5
     print('{} + {} = {}'.format(a, b, cal.add(a, b)))
