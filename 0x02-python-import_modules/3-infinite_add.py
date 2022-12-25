f0786b29a7101f9284427b508ae65bb09fae88b8
Your Name <you@example.com>
2022-12-24 22:55:36 -0800

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv) - 1
+    counter = 1
+    s = 0
+    if length == 0:
+        print('{:d}'.format(length))
+    else:
+        while counter <= length:
+            s = s + int(sys.argv[counter])
+            counter + 1
+        print('{:d}'.format(s))
