fa9f0dba9871feba26ef44680460ffc0b9a57bfa
Your Name <you@example.com>
2022-12-24 22:29:36 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,10 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) <= 1:
-        print('0 argumets.')
+        print('0 arguments.')
+    elif len(sys.argv) == 2:
+        print('1 argument:')
+    else:
+        print('{:d} argumets:'.format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
-        if len(sys.argv) == 2:
-            print('1 argument:')
-        else:
-            print('{:d} argumets:'.format(len(sys.argv) - 1))
-
         print('{:d}: {:s}'.format(i, sys.argv[i]))
