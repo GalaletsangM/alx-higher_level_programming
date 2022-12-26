b397cb3f2114ba9a7aa5a21eb1a7fe7f038021df
Your Name <you@example.com>
2022-12-25 17:28:21 -0800

    retrieve element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return
-    elif idx != my_list[i]:
-        return
+        return None
+    elif idx > len(my_list) - 1:
+        return None
     else:
-        for i in range(len(my_list - 1)):
-            print('{:d}'.format(my_list[i]))
-element_at(my_list, idx)
+        return my_list[idx]
