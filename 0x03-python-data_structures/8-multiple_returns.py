283f9e9f7031c3c93177b9705e8d8daf5bc9b61e
Your Name <you@example.com>
2022-12-26 21:53:06 -0800

    returns a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence = NULL:
-        sentence(0) = None
-    return (length, sentence(0))
+    if sentence == NULL:
+        sentence[0] = None
+    return (length, sentence[0])
