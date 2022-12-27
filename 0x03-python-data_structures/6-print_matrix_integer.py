c1071a28b46d3107897fabb72768734c34bfb276
Your Name <you@example.com>
2022-12-26 20:39:28 -0800

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             if j != i[-1]:
                 print('{:d'.format(j), end="")
+            else:
+                print('{:d}'.format(j), end="")
         print()
