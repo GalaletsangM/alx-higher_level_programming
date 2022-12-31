26d99de945b06d5963f2219cfb09f98110ba4ed8
Your Name <you@example.com>
2022-12-30 22:25:08 -0800

    find all multiples of 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    n = len(my_list)
+    i = 1
+    while i <= n:
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+        i++
